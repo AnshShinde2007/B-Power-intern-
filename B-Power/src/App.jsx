dfa7efd5eaf8b65c72a5ab6d33e126336e8f2e79
Ansh's websites <anshshinde449@gmail.com>
2025-05-25 23:20:44 +0530

    small change

--- B-Power/src/App.jsx
+++ B-Power/src/App.jsx
@@ -1,16 +1,9 @@
-import './App.css';
 import HeroSection from './components/HeroSection';
-// import VideoSection from './components/VideoSection';
-// import ProductCarousel from './components/ProductCarousel';
-// import TrustSignals from './components/TrustSignals';
 
 function App() {
   return (
     <div className="App">
       <HeroSection />
-      {/* <VideoSection />
-      <ProductCarousel />
-      <TrustSignals /> */}
     </div>
   );
 }
